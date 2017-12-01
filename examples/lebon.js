2b0423de5807e60250124781343999eece4d8ad8
Nikolay Maltsev <nmaltsev@solit-clouds.ru>
2017-12-01 18:50:52 +0300

    upd

--- examples/lebon.js
+++ examples/lebon.js
@@ -5,6 +5,14 @@ const	$parseDocument = $xmlParser.parseDocument;
 
 var link = 'https://www.leboncoin.fr/locations/offres/provence_alpes_cote_d_azur/?th=1&location=Nice%2CAntibes%2006600%2CCagnes-sur-Mer%2006800&sqs=1&ros=1&ret=2';
 
+const entitiesMap = {
+	'&amp;': '&',
+};
+
+function escapeHtmlEntities(str){
+	return str.replace(/&amp;/ig, '&');
+}
+
 class PageCollector{
 	// @param {Object} $request
 	// @param {Object} $parser
@@ -72,7 +80,7 @@ class PageCollector{
 				let nextLink = doc.querySelector('#next');
 
 				if(nextLink){
-					nextLink = nextLink.getAttribute('href');
+					nextLink = escapeHtmlEntities(nextLink.getAttribute('href'));
 					console.log('nextLink: %s', nextLink);
 					linkModel = new this.$request.UniversalLink(nextLink);
 					linkModel.inherit(this.location);
@@ -89,7 +97,35 @@ class PageCollector{
 			}else{
 				return true;	
 			}
-		})
+		});
+	}
+	// @param {():Object => {}} onnext
+	// @param {(Object) => {}} oncomplete
+	// @param {Array?} report
+	proceedPages(onnext, oncomplete, report){
+		var link = this.links.shift();
+		var report = report || [];
+
+		if(link){
+			this.$request.fetch(this.$request.getUriConfig('GET', link, {
+				Connection: 'keep-alive',
+				Accept: '*/*',
+				// 'Referer': 'https://addons.mozilla.org/ru/firefox/',
+				// 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+				'Accept-Encoding': 'gzip, deflate, sdch',
+				'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+				'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',	
+			}), (d) => {
+				report.push(onnext(d));
+				// continue
+			}, (e) => {
+				console.warn('Troubles while downloading: %s', link);
+				console.dir(e);
+				// Report about error and continue
+			})
+		}else{
+			oncomplete();
+		}
 	}
 }
 
