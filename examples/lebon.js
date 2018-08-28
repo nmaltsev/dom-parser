96da9e256a80f8c93ef6940f3ddbef18471cc9b3
Nicolas Maltsev <nsmalcev@gmail.com>
2018-08-28 22:48:29 +0300

    update lebon example

--- examples/lebon.js
+++ examples/lebon.js
@@ -5,9 +5,6 @@ const 	$iconv = require('iconv').Iconv;
 const 	$fs = require('fs');
 const 	$literalCompiler = require('./../src/literal_compiler');
 
-var link = 'https://www.leboncoin.fr/recherche/?category=10&regions=21&cities=Nice_06000,Nice_06200,Antibes_06600,Biot_06410&real_estate_type=2&furnished=1&price=300-850&rooms=1-2&square=20-50';
-
-
 /*
 Test case
 
@@ -61,15 +58,17 @@ class PageCollector{
 		this.$translator = $translator;
 		this.$timeFormatter = $timeFormatter;
 		this.$helpers = $helpers;
-		this.links = [];
 	}
 	// @param {String|UniversalLink} link
-	download(link){
+	// @param {string[]} links
+	download(link, links){
+		var _location;
 		if(link instanceof this.$request.UniversalLink){
-			this.location = link;
+			_location = link;
 		}else{
-			this.location = new this.$request.UniversalLink(link);	
+			_location = new this.$request.UniversalLink(link);	
 		}
+		var _links = links || [];
 
 		return this.$request.petch(this.$request.getUriConfig('GET', link, {
 			Connection: 'keep-alive',
@@ -81,7 +80,6 @@ class PageCollector{
 			'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',	
 		})).then((d) => {
 			var 	doc = this.$parser.parseDocument(d.body.toString(), {isHtml: true}),
-			 		// links = doc.querySelectorAll('.tabsContent>ul>li>a');
 			 		links = doc.querySelectorAll('[itemtype="http://schema.org/Offer"]>a');
 
 			// var 	now = new Date(),
@@ -104,8 +102,6 @@ class PageCollector{
 				if(date_s = links[i].querySelector('[itemprop="availabilityStarts"]')){
 					date_s = date_s.getAttribute('content').toLowerCase();
 
-
-					// if(date != currentDate){ // when we find another date
 					if (date_s.indexOf('aujourd') == -1 && date_s.indexOf('hier') == -1) {
 						isCompleted = true;
 					}
@@ -113,19 +109,16 @@ class PageCollector{
 					isCompleted = true;
 				}
 
-				// Aujourd'hui, 10:03, hier
-
-				console.log('Link: %s, date: %s', link, date_s || '-');
-				
 				linkModel = new this.$request.UniversalLink(link);
-				linkModel.inherit(this.location);
-				this.links.push(linkModel.toString());
+				linkModel.inherit(_location);
+				//this.links.push(linkModel.toString());
+				_links.push(linkModel.toString());
 
 				// console.log('D: %s, link: %s', date, link);
 				// console.log('M: %s', linkModel);
 			}
 
-			console.log('Founded links: %s, isCompleted: %s', links.length, isCompleted);
+			console.log('Founded links: %s, isCompleted: %s', _links.length, isCompleted);
 
 			if (!isCompleted) {
 				let nextLink = doc.querySelector('[name="chevronRight"]');
@@ -133,23 +126,23 @@ class PageCollector{
 				if(nextLink = nextLink.parentNode){
 					nextLink = pageCollector.$helpers.escapeHtmlEntities(nextLink.getAttribute('href'));
 					linkModel = new this.$request.UniversalLink(nextLink);
-					linkModel.inherit(this.location);
+					linkModel.inherit(_location);
 					nextLink = linkModel.toString();
 
 					console.log('Continue: %s', nextLink);
 
-					return this.download(nextLink);
+					return this.download(nextLink, _links);
 				}				
 			}else{
-				return true;	
+				return _links;	
 			}
 		});
 	}
 	// @param {():Object => {}} onnext
 	// @param {(Object) => {}} oncomplete
 	// @param {Array?} report
-	proceedPages(onnext, oncomplete, report){
-		var 	link = this.links.shift();
+	proceedPages(links, onnext, oncomplete, report){
+		var 	link = links.shift();
 		var 	report = report || [];
 
 		if(link){
@@ -164,12 +157,12 @@ class PageCollector{
 			}), (d) => {
 				report.push(onnext(link, d));
 				// continue
-				this.proceedPages(onnext, oncomplete, report);
+				this.proceedPages(links, onnext, oncomplete, report);
 			}, (e) => {
 				console.warn('Troubles while downloading: %s', link);
 				console.dir(e);
 				// Report about error and continue
-				this.proceedPages(onnext, oncomplete, report);
+				this.proceedPages(links, onnext, oncomplete, report);
 			});
 		}else{
 			oncomplete(report);
@@ -212,79 +205,104 @@ let pageCollector = new PageCollector(
 					return _entitiesMap[char];
 				});
 			}
-		}())
+		}()),
+		flatten: function(collection){
+			if (Array.isArray(collection)) {
+				let len = collection.length;
+
+				if (len < 2) {
+					return collection[0];
+				} else {
+					let out = [];
+
+					for(let i =0; i < len; i++) {
+						out = out.concat(collection[i]);
+					}
+					return out;
+				}
+			} else {
+				return collection;
+			}
+		}
 	}
 );
 
 
+Promise.all([
+	'https://www.leboncoin.fr/recherche/?category=10&regions=21&cities=Nice_06000,Nice_06200,Antibes_06600,Juan-les-Pins_06160&furnished=1&real_estate_type=2,1&price=300-900&rooms=1-3&square=25-60',
+	'https://www.leboncoin.fr/recherche/?category=10&regions=21&cities=Villeneuve-Loubet_06270,Cagnes-sur-Mer_06800,Biot_06410&furnished=1&real_estate_type=2,1&price=300-900&rooms=1-3&square=25-60'
+].map((link_s) => pageCollector.download(link_s))).then(function(results){
+	var links = pageCollector.$helpers.flatten(results);
+	
+	console.log('Collected %s links', links.length);
+	pageCollector.proceedPages(
+		links,
+		function(link, d){
+			// Prepare response body
+			var		body;
 
-// TODO refactor that code
-pageCollector.download(link).then(function(){
-	console.log('Founded %s links', pageCollector.links.length);
+			// Try to convert at unicode
+			var 	contentType = d.response.headers['content-type'] || '',
+					charsetTypeMatch = /charset=([^;]+)/ig.exec(contentType),
+					charset = charsetTypeMatch && charsetTypeMatch[1];
 
-	pageCollector.proceedPages(function(link, d){
-		// Prepare response body
-		var		body;
+			if(charset == 'windows-1252'){
+				body = pageCollector.$translator.convert(d.body).toString();
+			}else{
+				body = d.body.toString();
+			}
 
-		// Try to convert at unicode
-		var 	contentType = d.response.headers['content-type'] || '',
-				charsetTypeMatch = /charset=([^;]+)/ig.exec(contentType),
-				charset = charsetTypeMatch && charsetTypeMatch[1];
+			console.log('Downloaded: %s, %s charset: %s', link, body.length, charset);
 
-		if(charset == 'windows-1252'){
-			body = pageCollector.$translator.convert(d.body).toString();
-		}else{
-			body = d.body.toString();
+			// Parse document
+			var 	doc = pageCollector.$parser.parseDocument(body, {isHtml: true}),
+					key_s,
+	 				properties = {};
+
+	 		properties['description'] = doc.querySelector('[data-qa-id="adview_description_container"]>div:nth-child(1)');
+	 		properties['cost'] = doc.querySelector('[data-qa-id="adview_price"]');
+	 		properties['surface'] = doc.querySelector('[data-qa-id="criteria_item_square"]>div>div:last-child');
+	 		properties['rooms'] = doc.querySelector('[data-qa-id="criteria_item_rooms"]>div>div:last-child');
+	 		properties['isMeuble'] = doc.querySelector('[data-qa-id="criteria_item_furnished"]>div>div:last-child');
+	 		properties['city'] = doc.querySelector('[data-qa-id="adview_location_informations"]>span');
+
+	 		for (key_s in properties) {
+	 			if (properties.hasOwnProperty(key_s)) {
+	 				if (properties[key_s]) {
+	 					properties[key_s] = properties[key_s].getTextContent();
+	 				} else {
+	 					console.warn('There is no `%s` at downloaded page', key_s);
+	 				}
+	 			}
+	 		}
+
+
+	 		// let 	lng = (pageCollector.$helpers.getFirstMatch(/var\s+lng\s*=\s*"([^\"]+)"/i, body) || '').trim(),
+	 		// 		lat = (pageCollector.$helpers.getFirstMatch(/var\s+lat\s*=\s*"([^\"]+)"/i, body) || '').trim();
+
+			return {
+				link,
+				properties,
+				// lng,
+				// lat
+			};
+		}, 
+		function(report){
+			console.log('[Report]');
+			// ';var data = ' + + ';'
+			let filePath = './examples/data.json';
+
+			filePath = '../../_projects/lebon/lebon-app/src/assets/data.json';
+
+			// console.log('Report');
+			// console.dir(report);
+
+			$fs.writeFile( filePath,  JSON.stringify(report, null, '\t'), function(err) {
+			    if(err){
+			        return console.log(err);
+			    }
+			}); 
 		}
+	);
+})
 
-		console.log('Downloaded: %s, %s charset: %s', link, body.length, charset);
-
-		// Parse document
-		var 	doc = pageCollector.$parser.parseDocument(body, {isHtml: true}),
-				key_s,
- 				properties = {};
-
- 		properties['description'] = doc.querySelector('[data-qa-id="adview_description_container"]>div:nth-child(1)');
- 		properties['cost'] = doc.querySelector('[data-qa-id="adview_price"]');
- 		properties['surface'] = doc.querySelector('[data-qa-id="criteria_item_square"]>div>div:last-child');
- 		properties['rooms'] = doc.querySelector('[data-qa-id="criteria_item_rooms"]>div>div:last-child');
- 		properties['isMeuble'] = doc.querySelector('[data-qa-id="criteria_item_furnished"]>div>div:last-child');
- 		properties['city'] = doc.querySelector('[data-qa-id="adview_location_informations"]>span');
-
- 		for (key_s in properties) {
- 			if (properties.hasOwnProperty(key_s)) {
- 				if (properties[key_s]) {
- 					properties[key_s] = properties[key_s].getTextContent();
- 				} else {
- 					console.warn('There is no `%s` at downloaded page', key_s);
- 				}
- 			}
- 		}
-
-
- 		// let 	lng = (pageCollector.$helpers.getFirstMatch(/var\s+lng\s*=\s*"([^\"]+)"/i, body) || '').trim(),
- 		// 		lat = (pageCollector.$helpers.getFirstMatch(/var\s+lat\s*=\s*"([^\"]+)"/i, body) || '').trim();
-
-		return {
-			link,
-			properties,
-			// lng,
-			// lat
-		};
-	}, function(report){
-		console.log('[Report]');
-		// ';var data = ' + + ';'
-		let filePath = './examples/data.json';
-
-		filePath = '../../_projects/lebon/lebon-app/src/assets/data.json';
-
-		// console.log('Report');
-		// console.dir(report);
-
-		$fs.writeFile( filePath,  JSON.stringify(report, null, '\t'), function(err) {
-		    if(err){
-		        return console.log(err);
-		    }
-		}); 
-	});
-});
